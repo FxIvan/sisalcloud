6d424cccabb589793d4d83307d89a6cbb468a60f
Ivan <almendraivan210814@gmail.com>
2023-04-28 20:16:57 -0300

    peticiones get que devuelve un json

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,7 @@ FROM golang:alpine AS build
 ENV GOPROXY=https://proxy.golang.org
 
 WORKDIR /go/src/excelgo
+COPY . .
 COPY cmd/myapp/ . 
 
 RUN GOOS=linux go build -o /go/bin/excelgo server.go
--- cmd/myapp/server.go
+++ cmd/myapp/server.go
@@ -1,19 +1,38 @@
 package main
 
-import (
+import(
 	"net/http"
+	"github.com/gin-gonic/gin"
 )
 
-func main() {
+//Definicion de structura de JSON
+type album struct{
+	ID		string	`json:"id"`
+	Title	string	`json:"title"`
+	Artist	string	`json:"artist"`
+	Price	float64 		`json:"price"`
+}
 
-	
-	fs := http.FileServer(http.Dir("../../static/"))
-	http.Handle("/static/", http.StripPrefix("/static/",fs))
-	
+var albums = []album{
+	{ID:"1", Title:"Without Me", Artist:"Eminem", Price: 10},
+	{ID:"2", Title:"Normal", Artist:"Feid", Price: 9.99},
+	{ID:"3", Title:"Fragil", Artist:"Yahritza y Su Esenncia", Price: 15},
+	{ID:"4", Title:"Por ti", Artist:"Luana", Price: 8.99},
+	{ID:"5", Title:"Gata Caliente", Artist:"LOLO OG y Omar varela", Price: 6.85},
+	{ID:"6", Title:"TQG", Artist:"KAROL G Y Shakira", Price: 14.99},
+}
 
+func getAlbums(c *gin.Context){
+	//Envia la respuesta en json
+	c.IndentedJSON(http.StatusOK, albums)
+}
 
-	http.ListenAndServe("127.0.0.1:8080", nil)
+func main(){
+	//En este caso ponemos gin.Default() para que nos permite manejar las peticiones entrantes y decir que funcion tiene que hacer ante un peticion
+	//Puede manejar GET, POST, PUT, PATCH, DELETE, OPTIONS, HEAD
+	router := gin.Default()
+	router.GET("/albums", getAlbums)
 
-
-
+	//Corre el servidor en el puerto 8080
+	router.Run("127.0.0.1:8080")
 }
